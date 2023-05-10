90cbf2b5e1e155e1bcf594ff6b443e81b96154c0
Eloim2023 <124946573+Eloim2023@users.noreply.github.com>
2023-05-10 00:12:51 -0700

    Add files via upload

---
+++ src/components/quantityPicker.jsx
@@ -0,0 +1,36 @@
+import "./quantityPicker.css";
+import { useState } from "react";
+
+
+function QuantityPicker(props){
+
+    let [quantity, setQuantity] = useState(1);
+    function increase(){
+        console.log("button click");
+        let val = quantity + 1;
+        setQuantity (val);
+        props.onQuantiyChange(val);
+    }
+
+    function decrease(){
+        if (quantity === 1) return;
+        console.log("button click");
+        let val = quantity - 1;
+        setQuantity (val);
+        props.onQuantiyChange(val);
+
+    }
+
+return (
+    <div className="qt-picker">
+        <button className="btn btn-primary" disabled = {quantity === 10} onClick={increase}>+</button>
+        <label>{quantity}</label>
+        <button className="btn btn-primary" disabled = {quantity === 1} onClick={decrease}>-</button>
+
+    </div>
+
+);
+
+}
+
+export default QuantityPicker;
